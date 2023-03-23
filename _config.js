0957f0059d153b361f0d2c600de70f9f7b824531
Gloria Mwongeli Mutie <gmutie@safaricom.co.ke>
2023-03-23 20:49:39 +0300

    edit package json file

--- _config.js
+++ _config.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 var config = {}
 
 // Update to have your correct username and password
+//UAT
 config.mongoURI = {
-
-    production: 'mongodb+srv://gloriamutie:hBNUPsZYzdOq6rck@devops1.qwsflap.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://gloriamutie:hBNUPsZYzdOq6rck@devops1.qwsflap.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://gloriamutie:hBNUPsZYzdOq6rck@devops1.qwsflap.mongodb.net/?retryWrites=true&w=majority',
-
+    production: `mongodb+srv://${process.env.username}:${process.env.password}@nodeepressproject.lhdzplz.mongodb.net/darkroom?retryWrites=true&w=majority`,
+    development: `mongodb+srv://${process.env.username}:${process.env.password}@nodeepressproject.lhdzplz.mongodb.net/darkroom-dev?retryWrites=true&w=majority`,
+    test: `mongodb+srv://${process.env.username}:${process.env.password}@nodeepressproject.lhdzplz.mongodb.net/darkroom-test?retryWrites=true&w=majority`,
 }
 module.exports = config;
+
+
+// for prod use ${{ secrets.MONGO_USERNAME }}:${{ secrets.MONGO_PASSWORD }} 
+// for UAT use ${process.env.username}:${process.env.password}
